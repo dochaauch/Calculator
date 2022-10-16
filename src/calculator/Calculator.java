c4a94f8cae1b0fb96a3d8c5de920d32e66ba9d65
Jelena Jakobson <lena.docha@gmail.com>
2022-10-16 11:32:32 +0200

    refactor: divide into 2 classes

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,43 @@
+package src.calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    protected int num1;
+    protected int num2;
+    protected String operation;
+
+    double getResult(int num1, int num2, String operation) {
+        if ("-".equals(operation)) {
+            return num1 - num2;
+        } else if ("*".equals(operation)) {
+            return num1 * num2;
+        } else if ("+".equals(operation)) {
+            return num1 + num2;
+        } else if ("/".equals(operation)) {
+            return num1 / num2;
+        } else {
+            System.out.println("Операция не найдена");
+            return 0;
+        }
+    }
+
+    public void inputOutput() {
+        boolean flag = true;
+        while (flag) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите первое целое число: ");
+            int num1 = scanner.nextInt();
+            System.out.println("Введите знак операции (+-*/): ");
+            String operation = scanner.next();
+            System.out.println("Введите второе целое число: ");
+            int num2 = scanner.nextInt();
+            System.out.println(num1 + " " + operation + " " + num2 + " = " + getResult(num1, num2, operation));
+            System.out.println("Закончить программу? Нажмиже 'y', если да, иначе любую другую букву");
+            char checkPoint = scanner.next().charAt(0);
+            if (checkPoint == 'y') {
+                flag = false;
+            }
+        }
+    }
+}
---
+++ src/calculator/Main.java
@@ -0,0 +1,8 @@
+package src.calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.inputOutput();
+    }
+}
